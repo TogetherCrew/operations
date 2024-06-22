af018ce8d7b7e190e45b329bd67f7f014de6955e
Cyrille Derche <c.derche@me.com>
2024-06-22 13:10:50 +0200

    update init.js

--- compose/mongo/init.js
+++ compose/mongo/init.js
@@ -1,6 +1,6 @@
 console.log("Initializing database with init.js")
-var rootUser = _getEnv('MONGO_INITDB_ROOT_USERNAME');
-var rootPassword = _getEnv('MONGO_INITDB_ROOT_PASSWORD');
+var rootUser = process.env.MONGO_INITDB_ROOT_USERNAME;
+var rootPassword = process.env.MONGO_INITDB_ROOT_PASSWORD;
 var admin = db.getSiblingDB('admin');
 admin.auth(rootUser, rootPassword);
 db.createUser({ user: rootUser, pwd: rootPassword, roles: ["readWrite"] });
