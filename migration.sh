bae3a148ae18df328158da55ec88fb7bb8426b27
Cyrille Derche <c.derche@me.com>
2024-06-23 13:17:02 +0200

    migration updates

--- migration.sh
+++ migration.sh
@@ -16,41 +16,41 @@ declare -a paths
 
 # source volume
 volumes=(
-  # mongodb_data_container
-  # rmq_data_container
-  # neo4j_data
-  # neo4j_import
-  # neo4j_plugins
-  # grafana_volume
-  # prometheus_volume
-  # loki_volume
-  # # pgvector_data
-  # airflow_config
-  # airflow_logs
-  # airflow_plugins
-  # airflow_sources
-  # tempo_data
-  # qdrant_data
+  mongodb_data_container
+  rmq_data_container
+  neo4j_data
+  neo4j_import
+  neo4j_plugins
+  grafana_volume
+  prometheus_volume
+  loki_volume
+  pgvector_data
+  airflow_config
+  airflow_logs
+  airflow_plugins
+  airflow_sources
+  tempo_data
+  qdrant_data
   qdrant_snapshots
 )
 
 # target path
 paths=(
-  # /data/db/
-  # /var/lib/rabbitmq/
-  # /data/
-  # /import/
-  # /plugins/
-  # /var/lib/grafana/
-  # /prometheus/
-  # /data/loki/
-  # # /var/lib/postgresql/data/
-  # /opt/airflow/config/
-  # /opt/airflow/logs/
-  # /opt/airflow/plugins/
-  # /sources/
-  # /tmp/tempo/
-  # /qdrant/storage/
+  /data/db/
+  /var/lib/rabbitmq/
+  /data/
+  /import/
+  /plugins/
+  /var/lib/grafana/
+  /prometheus/
+  /data/loki/
+  /var/lib/postgresql/data/
+  /opt/airflow/config/
+  /opt/airflow/logs/
+  /opt/airflow/plugins/
+  /sources/
+  /tmp/tempo/
+  /qdrant/storage/
   /qdrant/snapshots/
 )
 
@@ -78,9 +78,12 @@ else
     echo 1. Downloading...
     ssh -i "$pem" $source "sudo docker run --rm -v development_$volume:$path ubuntu tar -cvf - -C $path ." > ./backup/$volume.tar
     
-    echo 3. Migrating...
+    echo 2. Migrating...
     docker run --rm -v compose_$volume:$path -v ./backup/:/backup/ ubuntu bash -c "cd $path && tar -xvf ../../backup/$volume.tar --strip 1"
     
+    echo 3. Deleting .tar
+    rm ./backup/$volume.tar
+
     echo 4. Completed
   done
 fi
